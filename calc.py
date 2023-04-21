f8439efe160467aa152f793060ffb4673138c227
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-04-21 10:18:04 -0400

    adding calc

--- calc.py
+++ calc.py
@@ -1,3 +1,3 @@
 FIRST_NUM = float(input("What is your first num? "))
 SECOND_NUM = float(input("What is your second num? "))
-SUM = print(FIRST_NUM + SECOND_NUM)
+SUM = print("Sum: " + str(FIRST_NUM + SECOND_NUM))
