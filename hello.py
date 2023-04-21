c692ec5789042d7cd58ec4eee29330f73e53b9de
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-04-21 09:26:25 -0400

    testing input

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print('testing')
+PATIENT_FIRST_NAME =  'John'
+PATIENT_LAST_NAME = 'Smith'
+PATIENT_AGE = 20
+IS_NEW_PATIENT = True
+print(PATIENT_FIRST_NAME)
---
+++ input.py
@@ -0,0 +1,2 @@
+FIRST_NAME = input("What is your name? ")
+print("Hello " + FIRST_NAME)
