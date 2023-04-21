1d42f49c862352be9a9fa8d565f9836a3f58cf99
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-04-21 13:12:49 -0400

    make negative kata solution

---
+++ functions/make_negative.py
@@ -0,0 +1,12 @@
+def make_negative(n):
+    if n == 0:
+        print("The value is zero")
+        # return 0
+    elif n > 0:
+        print("-" + str(n))
+    elif n < 0:
+        print("The value is negative")
+        # return n
+
+
+make_negative(5)
