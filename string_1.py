8303e8b8b51aedec2fc1468fcb9a98bd9419c860
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-04-21 10:27:30 -0400

    string methods

---
+++ string_1.py
@@ -0,0 +1,8 @@
+COURSE = "Python for Beginners"
+print(COURSE.upper())
+print(COURSE.lower())
+print(COURSE.find('f'))
+print(COURSE.find("Beg"))
+print(COURSE.replace('for', '4'))
+print(COURSE.replace("for", "4"))
+print("Python" in COURSE)
