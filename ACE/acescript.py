89424fe86c9d5a9f05b0f5325d5b3a19be19439b
jjuste-nyspi <jean.juste@nyspi.columbia.edu>
2023-05-23 09:53:42 -0400

    string to int solution

--- ACE/acescript.py
+++ ACE/acescript.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import requests
 import json
+
 data = {
     'token': '21678CA785CE821AADC6587B07643466',
     'content': 'record',
@@ -23,20 +24,13 @@ data = {
 }
 r = requests.post('https://rc-1.nyspi.org/api/',data=data)
 print('HTTP Status: ' + str(r.status_code))
-#print(r.json())
+print(r.json())
 
-#r.json is class 'method'
-#print(type(r.json))
+# records containing null program id's
+nullprogid = json.dumps(r.json())
 
-#convert my class method to json string
-jsonstr1 = json.dumps(r.json())
-
-#print(jsonstr1)
-#print(type(jsonstr1))
-
-
-#convert json string to json object
-jsonObject = json.loads(jsonstr1)
+# convert json string to json object
+jsonObject = json.loads(nullprogid)
 #print(jsonObject)
 #print(type(jsonObject))
 
@@ -45,6 +39,29 @@ for obj in jsonObject:
     obj["prog_id"] = 'PROG_' + obj["record_id"]
 
 print(jsonObject)
+print(type(jsonObject))
+
+
+recjson = json.dumps(jsonObject)
+
+data = {
+    'token': '21678CA785CE821AADC6587B07643466',
+    'content': 'record',
+    'action': 'import',
+    'format': 'json',
+    'type': 'flat',
+    'overwriteBehavior': 'normal',
+    'forceAutoNumber': 'false',
+    'data': recjson,
+    'returnContent': 'count',
+    'returnFormat': 'json'
+}
+r = requests.post('https://rc-1.nyspi.org/api/',data=data)
+print('HTTP Status: ' + str(r.status_code))
+print(r.json())
+
+
+
 
 
 
